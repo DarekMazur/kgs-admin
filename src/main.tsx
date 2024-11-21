7b2c66cdc977ded25c8453af6454d22ebc400c88
Darek Mazur <dev@darekmazur.it>
2024-11-21 05:14:42 +0100

    Enable MSW in develop mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+async function enableMocking() {
+  if (import.meta.env.VITE_NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser.ts');
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+});
