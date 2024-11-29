df8231ed17195e6540a2b14e4a7202ba598d9264
Darek Mazur <dev@darekmazur.it>
2024-11-29 02:41:51 +0100

    Create basic unauthorised view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import UnauthorisedView from "./views/UnauthorisedView/UnauthorisedView.tsx";
+
 const App = () => {
+  const isAuthorised = false
   return (
+    <>
       <h1>Lorem Ipsum</h1>
+      {isAuthorised ? (<UnauthorisedView />) : <h3>Welcome</h3>}
+    </>
   )
 }
 
