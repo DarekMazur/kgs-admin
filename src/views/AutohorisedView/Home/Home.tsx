ff26f90b1c806452790ed941414994de8aba4523
Darek Mazur <dev@darekmazur.it>
2024-12-08 14:46:02 +0100

    Use MUI in home view

--- src/views/AutohorisedView/Home/Home.tsx
+++ src/views/AutohorisedView/Home/Home.tsx
@@ -1,12 +1,14 @@
 import { RootState, setGlobalUser, switchIsLoading, useGetUsersQuery } from '../../../../store'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, MouseEvent } from 'react'
-import { Button } from '@mui/material'
+import { Box, Button, Paper, Typography, List, ListItem } from '@mui/material'
 import { useAuth } from '../../../utils/hooks/useAuth.tsx'
 import Loader from '../../../components/Loader/Loader.tsx'
+import { IUser } from '../../../utils/types.ts'
 
 const Home = () => {
   const { data: users, isLoading: usersLoading } = useGetUsersQuery()
+  const globalUser: IUser | null = useSelector<RootState, IUser | null>((store) => store.globalUser)
   const dispatch = useDispatch()
   const isLoading = useSelector<RootState>((state) => state.isLoading)
   const { logout } = useAuth()
@@ -28,19 +30,27 @@ const Home = () => {
         <Loader />
       ) : (
         <>
-          <h3>Welcome</h3>
-          <Button type="button" variant="contained" color="primary" onClick={handleLogout}>
-            Wyloguj
-          </Button>
-          <ul>
-            {users
-              ? users.map((user) => (
-                  <li key={user.id}>
-                    {user.username} ({user.email}) - {user.id}
-                  </li>
-                ))
-              : null}
-          </ul>
+          {globalUser ? (
+            <Paper elevation={0}>
+              <Box sx={{ flexGrow: 1, m: '2rem 3rem' }}>
+                <Typography component="h3" variant="h2" color="secondary">
+                  Witaj, {globalUser.username}
+                </Typography>
+                <Button type="button" variant="contained" color="primary" onClick={handleLogout}>
+                  Wyloguj
+                </Button>
+                <List>
+                  {users
+                    ? users.map((user) => (
+                        <ListItem key={user.id}>
+                          {user.username} ({user.email}) - {user.id}
+                        </ListItem>
+                      ))
+                    : null}
+                </List>
+              </Box>
+            </Paper>
+          ) : null}
         </>
       )}
     </>
