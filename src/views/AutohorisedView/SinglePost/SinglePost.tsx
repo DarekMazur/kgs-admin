1c06f3027f8e12ad6f24746710e62b1c6d7e8618
Darek Mazur <dev@darekmazur.it>
2024-12-12 14:18:31 +0100

    Create single post route

---
+++ src/views/AutohorisedView/SinglePost/SinglePost.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from '@mui/material'
+
+const SinglePost = () => {
+  return (
+    <Container component="main">
+      <Typography variant="h3">Single post</Typography>
+    </Container>
+  )
+}
+
+export default SinglePost
