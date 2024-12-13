38bbc54d2d3442ab8fc5bb4252950eefe1ba6ada
Darek Mazur <dev@darekmazur.it>
2024-12-13 23:48:35 +0100

    Handle mark as read/unread switch

--- src/views/AutohorisedView/Messages/Messages.tsx
+++ src/views/AutohorisedView/Messages/Messages.tsx
@@ -1,4 +1,16 @@
-import { Avatar, Box, Container, Divider, List, ListItem, Paper, Typography } from '@mui/material'
+import {
+  Avatar,
+  Box,
+  ButtonGroup,
+  Container,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  Paper,
+  Tooltip,
+  Typography
+} from '@mui/material'
 import { RootState, setGlobalUser } from '../../../../store'
 import { useDispatch, useSelector } from 'react-redux'
 import { IMessage, IUser } from '../../../utils/types.ts'
@@ -6,11 +18,35 @@ import { formatDate } from '../../../utils/helpers/formatDate.ts'
 import { useState } from 'react'
 import InboxIcon from '@mui/icons-material/Inbox'
 import { theme } from '../../../utils/theme.tsx'
+import EmailIcon from '@mui/icons-material/Email'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const Messages = () => {
   const globalUser: IUser | null = useSelector<RootState, IUser | null>((state) => state.globalUser)
   const [openMessage, setOpenMessage] = useState<IMessage | null>(null)
   const dispatch = useDispatch()
+
+  const handleReadSwitch = (id: string) => {
+    if (globalUser) {
+      const message = globalUser.messages.inbox.find((message) => message.id === id)
+
+      if (message) {
+        let messages = { ...globalUser.messages }
+
+        messages = {
+          inbox: [
+            ...globalUser.messages.inbox.filter((message) => message.id !== id),
+            { ...message, openedTime: message.openedTime ? null : JSON.stringify(new Date()) }
+          ],
+          sent: [...globalUser.messages.sent]
+        }
+
+        const updatedUser = { ...globalUser, messages }
+
+        dispatch(setGlobalUser(updatedUser))
+      }
+    }
+  }
 
   const handleChoseMessage = (id: string) => {
     if (globalUser) {
@@ -110,10 +146,31 @@ const Messages = () => {
             </Box>
             {openMessage ? (
               <Box sx={{ width: '65%', height: '400px' }}>
-                <Typography variant="h3">{openMessage.header}</Typography>
-                <Typography variant="body1">
+                <Typography variant="body2" sx={{ width: '100%', textAlign: 'right' }}>
                   {formatDate(new Date(openMessage.sendTime), 'full')}
                 </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  Od: {openMessage.sender.username}
+                  <ButtonGroup disableElevation variant="contained" sx={{ mt: '1rem', mb: '2rem' }}>
+                    <Tooltip
+                      title={
+                        openMessage.openedTime
+                          ? 'Oznacz jako nieprzeczytane'
+                          : 'Oznacz jako przeczytane'
+                      }
+                    >
+                      <IconButton onClick={() => handleReadSwitch(openMessage.id)}>
+                        <EmailIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Usuń">
+                      <IconButton>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </ButtonGroup>
+                </Box>
+                <Typography variant="h3">{openMessage.header}</Typography>
                 {openMessage.message}
               </Box>
             ) : (
