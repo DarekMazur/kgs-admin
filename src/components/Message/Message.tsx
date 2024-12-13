9a4879b4aa19f30b8cde3f1f7e1889194882b314
Darek Mazur <dev@darekmazur.it>
2024-12-14 00:09:47 +0100

    Create component for no open message view

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -2,11 +2,11 @@ import { Box, ButtonGroup, IconButton, Tooltip, Typography } from '@mui/material
 import { formatDate } from '../../utils/helpers/formatDate.ts'
 import EmailIcon from '@mui/icons-material/Email'
 import DeleteIcon from '@mui/icons-material/Delete'
-import InboxIcon from '@mui/icons-material/Inbox'
 import { FC } from 'react'
 import { IMessage, IUser } from '../../utils/types.ts'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store'
+import NoOpenMessage from '../NoOpenMessage/NoOpenMessage.tsx'
 
 const Message: FC<{
   openMessage: IMessage | null
@@ -49,27 +49,7 @@ const Message: FC<{
               {openMessage.message}
             </Box>
           ) : (
-            <Box
-              sx={{
-                width: '65%',
-                height: '400px',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '1rem'
-              }}
-            >
-              <InboxIcon color="secondary" sx={{ width: '150px', height: '150px' }} />
-              <Typography variant="h3">
-                Witaj, {globalUser.firstName ? globalUser.firstName : globalUser.username}!
-              </Typography>
-              <Typography variant="body1">
-                {globalUser.messages.inbox.length > 0
-                  ? `masz ${globalUser.messages.inbox.length} wiadomości${globalUser.messages.inbox.filter((message) => !message.openedTime).length > 0 ? `, w tym ${globalUser.messages.inbox.filter((message) => !message.openedTime).length} nieprzeczytanych` : ''}`
-                  : 'nie masz żadnych wiadomości'}
-              </Typography>
-            </Box>
+            <NoOpenMessage />
           )}
         </>
       ) : (
