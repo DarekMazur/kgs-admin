c85edcd4aca0a8f663d14f2192361127e5388d67
Darek Mazur <dev@darekmazur.it>
2024-12-16 02:46:53 +0100

    Update store

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,7 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { usersApi } from './api/users.ts'
 import { IUser } from '../src/utils/types.ts'
 import { postsApi } from './api/posts.ts'
+import { messagesApi } from './api/messages.ts'
 
 export type RootState = ReturnType<typeof store.getState>
 
@@ -21,13 +22,18 @@ export const { setGlobalUser } = globalUserSlice.actions
 
 export * from './api/users.ts'
 export * from './api/posts.ts'
+export * from './api/messages.ts'
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
     globalUser: globalUserSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(usersApi.middleware).concat(postsApi.middleware)
+    getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(postsApi.middleware)
+      .concat(messagesApi.middleware)
 })
