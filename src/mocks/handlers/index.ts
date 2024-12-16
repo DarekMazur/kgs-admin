c7477bc871a669cb45e8fce2b95b4717f63dc9f1
Darek Mazur <dev@darekmazur.it>
2024-12-16 02:02:11 +0100

    Mock messages db

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -2,5 +2,12 @@ import { handlers as postHandlers } from './posts'
 import { handlers as peakHandlers } from './peaks'
 import { handlers as userHandlers } from './users'
 import { handlers as roleHandlers } from './roles'
+import { handlers as messageHandlers } from './messages'
 
-export const handlers = [...postHandlers, ...peakHandlers, ...userHandlers, ...roleHandlers]
+export const handlers = [
+  ...postHandlers,
+  ...peakHandlers,
+  ...userHandlers,
+  ...roleHandlers,
+  ...messageHandlers
+]
---
+++ src/mocks/handlers/messages.ts
@@ -0,0 +1,54 @@
+import { http, HttpResponse } from 'msw'
+import { db } from '../db.ts'
+import { v4 as uuidv4 } from 'uuid'
+
+export const handlers = [
+  http.get(`${import.meta.env.VITE_API_URL}/messages`, () => {
+    return HttpResponse.json(
+      db.message.getAll().sort((a, b) => b.sendTime.getTime() - a.sendTime.getTime())
+    )
+  }),
+
+  http.post(`${import.meta.env.VITE_API_URL}/messages`, async ({ request }) => {
+    const body = await request.json()
+
+    // @ts-ignore
+    const { header, message, sendTime, sender, recipient } = body
+
+    const userSender = db.user.findFirst({
+      where: {
+        id: {
+          equals: sender.id
+        }
+      }
+    })!
+
+    const userRecipient = db.user.findFirst({
+      where: {
+        id: {
+          equals: recipient.id
+        }
+      }
+    })!
+
+    const newMessage = {
+      id: uuidv4(),
+      header,
+      message,
+      sendTime,
+      openedTime: null,
+      sender: {
+        id: sender.id,
+        username: userSender.username,
+        role: userSender.role?.name
+      },
+      recipient: {
+        id: recipient.id,
+        username: userRecipient.username,
+        role: userRecipient.role?.name
+      }
+    }
+
+    return HttpResponse.json(newMessage, { status: 20 })
+  })
+]
