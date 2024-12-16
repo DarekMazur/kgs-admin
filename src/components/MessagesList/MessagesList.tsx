51bc2d4533e14ed4ca4fdc63ad6c027bf459c088
Darek Mazur <dev@darekmazur.it>
2024-12-16 13:45:41 +0100

    Fix messages sorting

--- src/components/MessagesList/MessagesList.tsx
+++ src/components/MessagesList/MessagesList.tsx
@@ -7,15 +7,18 @@ const MessagesList: FC<{ messages: IMessage[]; handleChoseMessage: (id: string) 
   messages,
   handleChoseMessage
 }) => {
+  const messagesList = structuredClone(messages).sort(
+    (a, b) => new Date(b.sendTime).getTime() - new Date(a.sendTime).getTime()
+  )
   return (
     <Box>
       <List sx={{ height: '500px', overflowY: 'auto' }}>
-        {messages.map((message, index) => (
+        {messagesList.map((message, index) => (
           <MessageListItem
             key={message.id}
             message={message}
             handleChoseMessage={handleChoseMessage}
-            isLast={index + 1 >= messages.length}
+            isLast={index + 1 >= messagesList.length}
           />
         ))}
       </List>
